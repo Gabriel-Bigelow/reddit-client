8f60740cc340e823eba58afdfc7aac4184772254
unknown <Gabriel Bigelow>
2022-10-22 22:13:09 -0400

    stylize Searchbar component and ready it for Redux functionality

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,14 +1,28 @@
 import './Searchbar.css'
+import { NavLink } from 'react-router-dom';
 import logo from './logo.png';
-import searchIcon from './searchIcon.png';
 
 export default function Searchbar () {
     return (
         <nav>
-            <img id="nav-logo" src={logo} alt="a blue reddit snoo"/>
-            <form>
-                <input id="searchbar" placeholder='Search' />
-            </form>
+            
+            <div class="inner" id="left-flex">
+                <img id="nav-logo" src={logo} alt="a blue reddit snoo"/>
+                <h1>Lurker</h1>
+            </div>
+            
+            
+            <div class="inner" id="center-flex">
+                <form>
+                    <input id="searchbar" placeholder='Search' />
+                </form>
+                <NavLink>Popular</NavLink>
+            </div>
+
+            <div class="inner" id="right-flex">
+                <NavLink>Notifications</NavLink>
+                <NavLink>Account</NavLink>
+            </div>
         </nav>
     )
 }
