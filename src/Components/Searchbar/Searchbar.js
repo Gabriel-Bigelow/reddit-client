0c93f400060d8d06c3e47da16256a8922b3b74d2
unknown <Gabriel Bigelow>
2022-10-22 22:50:11 -0400

    Typo: change 'class' to 'className' on SearchBar component

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -4,22 +4,22 @@ import logo from './logo.png';
 
 export default function Searchbar () {
     return (
-        <nav>
+        <nav id="nav-bar">
             
-            <div class="inner" id="left-flex">
+            <div className="inner" id="left-flex">
                 <img id="nav-logo" src={logo} alt="a blue reddit snoo"/>
-                <h1>Lurker</h1>
+                <h1 id="header">Lurker</h1>
             </div>
             
             
-            <div class="inner" id="center-flex">
+            <div className="inner" id="center-flex">
                 <form>
                     <input id="searchbar" placeholder='Search' />
                 </form>
                 <NavLink>Popular</NavLink>
             </div>
 
-            <div class="inner" id="right-flex">
+            <div className="inner" id="right-flex">
                 <NavLink>Notifications</NavLink>
                 <NavLink>Account</NavLink>
             </div>
